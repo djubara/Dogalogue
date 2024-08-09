e7b821bc019c66cff2210df220e0ea09ebe6f34f
atengelsgjerd <atengelsgjerd@gmail.com>
2024-08-08 21:45:25 -0500

    Added content to profile section

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,25 @@
+import Card from 'react-bootstrap/Card';
 const Profile = () => {
     return (
         <div className="profile">
             <h2>Profile</h2>
-            
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://placehold.co/600x400" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    
+                </Card.Body>
+            </Card>
+
         </div>
-     );
+    );
 }
- 
+
 export default Profile;
+
+
+
