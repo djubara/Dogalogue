7156c676bc2aa5e9dbbc1b9f8afb89631c14f05b
Cole Patterson <cole.patter@gmail.com>
2024-08-08 21:13:16 -0500

    create models & upgrade jsonwebtoken

---
+++ server/models/Owner.js
@@ -0,0 +1,36 @@
+import { model, Schema } from "mongoose";
+
+const ownerSchema = new Schema({
+    // meta
+    created: {
+        type: Date,
+        required: true,
+        unique: false,
+        default: Date.now
+    },
+    modified: {
+        type: Date,
+        required: true,
+        unique: false,
+        default: Date.now
+    },
+
+    // profile data
+    firstName: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    lastName: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    photoUrl: {
+        type: String,
+        required: true,
+        unique: false
+    }
+})
+
+const Owner = model("Owner", ownerSchema)
