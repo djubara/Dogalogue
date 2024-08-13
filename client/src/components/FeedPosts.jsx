d47261bdcb152a414c957ab08b781f35cf586b06
emulcahey <e.yehaclum@gmail.com>
2024-08-12 21:26:19 -0500

    changed style, borders and colors

--- client/src/components/FeedPosts.jsx
+++ client/src/components/FeedPosts.jsx
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom';
 import posts from "../../data/posts";
+import doggo from "../../public/assets/remi.jpg";
 
 const FeedPosts = () => {
     return ( 
         <div className="feed-posts">
             {posts.map((post) => (
-                <div className="post" key={post.id}>
+                <div className="post individualpostborder" key={post.id}>
                     <Link to={`/post/${post.id}`}>
-                        <h2>{post.title}</h2>
-                        <p>{post.content}</p>
+                        <div className="postbox align-items-center">
+                            <img className="postimage" src={doggo} />
+                            <div className="postcontent">
+                                <h2>{post.title}</h2>
+                                <p>{post.content}</p>
+                            </div>
+                        </div>
                     </Link>
                 </div>
             ))}
